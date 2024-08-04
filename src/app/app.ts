458532ec73a63540d3605f2c57ce07ca859b81c5
rRedq <redq_000@mail.ru>
2024-08-05 04:07:05 +0500

    refactor: update download icon

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,20 @@
-import { div, main } from 'shared/lib/dom/tag-function';
+import { div, img, main } from 'shared/lib/dom/tag-function';
 import { downloadPdf } from 'shared/lib/utils';
 import { bottomSection } from 'widgets/bottomSection';
 import { middleSection } from 'widgets/middleSection';
 import { upperSection } from 'widgets/upperSection';
 import style from './styles/app.module.scss';
+import icon from 'shared/assets/images/download.svg';
 import './styles/global.scss';
 
 export const createApp = () => {
-  const btn = div({
-    className: style.saveBtn,
-    textContent: 'Download PDF',
-    onclick: () => downloadPdf(app, 'cv'),
-  });
+  const btn = div(
+    {
+      className: style.saveBtn,
+      onclick: () => downloadPdf(app, 'cv'),
+    },
+    [img({ src: icon, alt: icon })]
+  );
 
   const app = main({ className: style.cv }, [upperSection(), middleSection(), bottomSection()]);
 
